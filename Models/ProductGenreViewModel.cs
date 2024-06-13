5c24a11004b1960fefa8973868a8192bb01d81be
Vandan Ashok Patel <W0857192@myscc.ca>
2024-06-13 13:40:45 -0400

    New Project

---
+++ Models/ProductGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public class ProductGenreViewModel
+{
+    public List<Product>? Products { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? ProductGenre { get; set; }
+    public string? SearchString { get; set; }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new WebApplication1Context(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<WebApplication1Context>>()))
+        {
+            // Look for any movies.
+            if (context.Project.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Product.AddRange(
+                new Product
+                {
+                    Title = "",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "SciFi",
+                    Rating = "R",
+                    Price = 7.99M
+                },
+                new Product
+                {
+                    Title = "",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "SciFi",
+                    Rating = "R",
+                    Price = 8.99M
+                },
+                new Product
+                {
+                    Title = "",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "SciFi",
+                    Rating = "R",
+                    Price = 9.99M
+                },
+                new Product
+                {
+                    Title = "",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "SciFi",
+                    Rating = "R",
+                    Price = 3.99M
+                },
+                new Product
+                {
+                    Title = "",
+                    ReleaseDate = DateTime.Parse("2024-1-2"),
+                    Genre = "Adventure",
+                    Rating = "R",
+                    Price = 5.70M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
